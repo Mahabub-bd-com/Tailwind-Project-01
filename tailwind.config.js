523d03f3b3fbbd9f7a639bf47a47259c65680cb4
Mahabub-bd-com <mahabub1625@gmail.com>
2024-07-08 10:12:27 +0600

    Tailwind CSS Project OK

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',            // use for darkMode
+  content: ['*'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
